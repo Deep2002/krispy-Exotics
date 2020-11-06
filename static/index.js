e43b454b8987a70b3ec8438360360036e87f2fe6
Deep <deepp4846@gmail.com>
2020-11-06 15:22:19 -0600

    img added manually

--- static/index.js
+++ static/index.js
@@ -1,55 +1,55 @@
-window.onscroll = function () { myFunction(), myFunction2() };
+// window.onscroll = function () { myFunction(), myFunction2() };
 
-var imgs = document.getElementsByTagName("img")
-
-
-function myFunction() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        imgs[0].className = "test";
-        imgs[1].className = "test";
-        imgs[2].className = "test";
-        imgs[3].className = "test";
-        imgs[4].className = "test";
-        imgs[5].className = "test";
-        imgs[6].className = "test";
-        imgs[7].className = "test";
-        imgs[8].className = "test";
-        imgs[9].className = "test";
-        imgs[10].className = "test";
-        imgs[11].className = "test";
-        imgs[12].className = "test";
-        imgs[13].className = "test";
-        imgs[14].className = "test";
-        imgs[15].className = "test";
-        imgs[16].className = "test";
-        imgs[17].className = "test";
-        imgs[18].className = "test";
-        imgs[19].className = "test";
-        imgs[20].className = "test";
+// var imgs = document.getElementsByTagName("img")
 
 
-    } else {
-        imgs[0].className = "remove-test";
-        imgs[1].className = "remove-test";
-        imgs[2].className = "remove-test";
-        imgs[3].className = "remove-test";
-        imgs[4].className = "remove-test";
-        imgs[5].className = "remove-test";
-        imgs[6].className = "remove-test";
-        imgs[7].className = "remove-test";
-        imgs[8].className = "remove-test";
-        imgs[9].className = "remove-test";
-        imgs[10].className = "remove-test";
-        imgs[11].className = "remove-test";
-        imgs[12].className = "remove-test";
-        imgs[13].className = "remove-test";
-        imgs[14].className = "remove-test";
-        imgs[15].className = "remove-test";
-        imgs[16].className = "remove-test";
-        imgs[17].className = "remove-test";
-        imgs[18].className = "remove-test";
-        imgs[19].className = "remove-test";
-        imgs[20].className = "remove-test";
+// function myFunction() {
+//     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//         imgs[0].className = "test";
+//         imgs[1].className = "test";
+//         imgs[2].className = "test";
+//         imgs[3].className = "test";
+//         imgs[4].className = "test";
+//         imgs[5].className = "test";
+//         imgs[6].className = "test";
+//         imgs[7].className = "test";
+//         imgs[8].className = "test";
+//         imgs[9].className = "test";
+//         imgs[10].className = "test";
+//         imgs[11].className = "test";
+//         imgs[12].className = "test";
+//         imgs[13].className = "test";
+//         imgs[14].className = "test";
+//         imgs[15].className = "test";
+//         imgs[16].className = "test";
+//         imgs[17].className = "test";
+//         imgs[18].className = "test";
+//         imgs[19].className = "test";
+//         imgs[20].className = "test";
 
-    }
-}
+
+//     } else {
+//         imgs[0].className = "remove-test";
+//         imgs[1].className = "remove-test";
+//         imgs[2].className = "remove-test";
+//         imgs[3].className = "remove-test";
+//         imgs[4].className = "remove-test";
+//         imgs[5].className = "remove-test";
+//         imgs[6].className = "remove-test";
+//         imgs[7].className = "remove-test";
+//         imgs[8].className = "remove-test";
+//         imgs[9].className = "remove-test";
+//         imgs[10].className = "remove-test";
+//         imgs[11].className = "remove-test";
+//         imgs[12].className = "remove-test";
+//         imgs[13].className = "remove-test";
+//         imgs[14].className = "remove-test";
+//         imgs[15].className = "remove-test";
+//         imgs[16].className = "remove-test";
+//         imgs[17].className = "remove-test";
+//         imgs[18].className = "remove-test";
+//         imgs[19].className = "remove-test";
+//         imgs[20].className = "remove-test";
+
+//     }
+// }
